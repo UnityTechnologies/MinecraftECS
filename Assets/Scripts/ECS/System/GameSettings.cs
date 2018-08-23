609931a12bad28f00a180a41a7ece397b940a6d5
Kelvin Lo <dada.artist@gmail.com>
2018-08-24 02:47:12 +0800

    Add tree generator function

--- Assets/Scripts/ECS/System/GameSettings.cs
+++ Assets/Scripts/ECS/System/GameSettings.cs
@@ -15,17 +15,18 @@ namespace Minecraft
         public static EntityArchetype BlockArchetype;
 
 
-        [Header("Block Info")]
-        public GameObject blockPrefab;
+        //[Header("Block Info")]
+        //public GameObject blockPrefab;
         //public GameObject spritePrefab;
         [Header("Chunk Info")]
         public int ChunkCount = 1;
-        //public int ChunkCountIncremement = 1;
 
-        [Header("Material Info")]
+        [Header("Mesh Info")]
         public Mesh blockMesh;
         public Mesh dirtMesh;
         //public Mesh tallgrassMesh;
+
+        [Header("Material Info")]
         public Material stoneMaterial;
         public Material brickMaterial;
         public Material cobbleMaterial;
@@ -36,9 +37,11 @@ namespace Minecraft
         //public Material roseMaterial;
         //public Material tallGrassMaterial;
 
-        public Material woodsideMaterial;
-        public Material woodtopMaterial;
-        public Material dirtwgrassMaterial;
+        public Material woodMaterial;
+        public Material leavesMaterial;
+        public Material surfaceMaterial;
+        [Header("")]
+        public Material pinkMaterial;
 
         /*
         public Material no1Mat;
@@ -49,9 +52,9 @@ namespace Minecraft
         public Material no6Mat;
         public Material no0Mat;
         public Material noQMat;
-*/
+        */
 
-        //int RanMat;
+        int ranDice;
         Material Matemp;
         Mesh Meshtemp;
 
@@ -77,22 +80,22 @@ namespace Minecraft
             BlockArchetype = manager.CreateArchetype(
                 typeof(TransformMatrix),
                 typeof(Position));
-             //typeof(MeshInstanceRenderer));
+            //typeof(MeshInstanceRenderer));
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         void Start()
         {
             manager = World.Active.GetOrCreateManager<EntityManager>();
-            GenerateChunk(ChunkCount);
-           //GenerateSprite(ChunkCount);
+            //Generate the world
+            ChunkGenerator(ChunkCount);
         }
 
-        void GenerateChunk(int amount)
+        void ChunkGenerator(int amount)
         {
 
             int totalamount = (amount * amount) * 1500;
-            int ordernumber = 0;
+            //int ordernumber = 0;
             int hightlevel;
             bool airChecker;
 
@@ -101,104 +104,147 @@ namespace Minecraft
 
 
 
-            for (int i = 0; i < amount; i++)
+            //for (int i = 0; i < amount; i++)
+            //{
+
+            //if (ordernumber == totalamount)
+            //return;
+
+            //Block ordering from X*0,0,0 to 15,10,10( * Chunk x2)
+            for (int yBlock = 0; yBlock < 15; yBlock++)
             {
-
-                if (ordernumber == totalamount)
-                    return;
-
-                //Block ordering from X*0,0,0 to 10,10,10( * ChunkCount)
-                for (int yBlock = 0; yBlock < 15; yBlock++)
+                for (int xBlock = 0; xBlock < 10 * amount; xBlock++)
                 {
-                    for (int xBlock = 0; xBlock < 10 * amount; xBlock++)
+                    for (int zBlock = 0; zBlock < 10 * amount; zBlock++)
                     {
-                        for (int zBlock = 0; zBlock < 10 * amount; zBlock++)
+                        hightlevel = (int)(Heightmap.GetPixel(xBlock, zBlock).r * 100) - yBlock;
+                        airChecker = false;
+
+                        switch (hightlevel)
                         {
-                            hightlevel = (int)(Heightmap.GetPixel(xBlock, zBlock).r * 100) - yBlock;
-                            airChecker = false;
+                            case 0:
+                                //random surface block
+                                ranDice = Random.Range(1, 201);
+                                if (ranDice <= 20)
+                                {
+                                    //grass
 
-                            switch (hightlevel)
+                                }
+                                if (ranDice == 200)
+                                {
+                                    //rose
+
+                                }
+                                if (ranDice == 199)
+                                {
+                                    //tree
+                                    TreeGenerator(xBlock, yBlock, zBlock);
+
+
+                                }
+                                airChecker = true;
+                                break;
+                            case 1:
+                                Meshtemp = dirtMesh;
+                                Matemp = surfaceMaterial;
+                                break;
+                            case 2:
+                            case 3:
+                            case 4:
+                                //Dirt
+                                Meshtemp = blockMesh;
+                                Matemp = dirtMaterial;
+                                break;
+                            case 5:
+                            case 6:
+                                //stone block
+                                Meshtemp = blockMesh;
+                                Matemp = stoneMaterial;
+                                break;
+                            case 7:
+                            case 8:
+                                Meshtemp = blockMesh;
+                                Matemp = cobbleMaterial;
+                                break;
+                            default:
+                                //airBlock or anything hight level < 0
+                                airChecker = true;
+
+                                break;
+
+                        }
+
+                        if (!airChecker)
+                        {
+
+                            if (!Matemp)
+                                Matemp = pinkMaterial;
+
+                            Entity entities = manager.CreateEntity(BlockArchetype);
+                            manager.SetComponentData(entities, new Position { Value = new int3(xBlock, yBlock, zBlock) });
+                            //manager.SetSharedComponentData(entities[ordernumber], new MeshInstanceRenderer
+                            //{
+                            //  mesh = Meshtemp,
+                            //material = Matemp
+                            //});
+
+                            manager.AddSharedComponentData(entities, new MeshInstanceRenderer
                             {
-                                case 0:
-                                    //Meshtemp = tallgrassMesh;
-                                    //Matemp = tallGrassMaterial;
-                                    /*
-                                    RanMat = Random.Range(1, 201);
-                                    if (RanMat <= 20)
-                                    {
-                                        //Matemp = grassTopMaterial;
+                                mesh = Meshtemp,
+                                material = Matemp
+                            });
 
-                                    }
-                                    if (RanMat == 200)
-                                    {
-                                        //Matemp = roseMaterial;
+                        }
 
-                                    }
-                                    if (RanMat == 199)
-                                    {
-                                        //Matemp = woodsideMaterial;
+                        //ordernumber += 1;
+                    }
+                }
+            }
+            //}
+            //entities.Dispose();
+        }
+        void TreeGenerator(int xPos, int yPos, int zPos)
+        {
+            //xpos,ypos,zpos is the root position of the tree that we are going to plant.
+            //woods
+            for (int i = yPos; i < yPos + 7; i++)
+            {
+                //top leaves
+                if (i == yPos + 6)
+                {
+                    //woodMaterial = leavesMaterial;
+                }
 
-                                    }*/
-                                    //break;
-                                    //surface
-                                    airChecker = true;
-                                    //Meshtemp = blockMesh;
-                                    //Matemp = no0Mat;
-                                    break;
-                                case 1:
-                                    Meshtemp = dirtMesh;
-                                    Matemp = dirtwgrassMaterial;
-                                    break;
-                                case 2:
-                                case 3:
-                                case 4:
-                                    //Dirt
-                                    Meshtemp = blockMesh;
-                                    Matemp = dirtMaterial;
-                                    break;
-                                case 5:
-                                case 6:
-                                    //stone block
-                                    Meshtemp = blockMesh;
-                                    Matemp = stoneMaterial;
-                                    break;
-                                case 7:
-                                case 8:
-                                    Meshtemp = blockMesh;
-                                    Matemp = cobbleMaterial;
-                                    break;
-                                default:
-                                    //airBlock or any highlevel < 0
-                                    airChecker = true;
-                                
-                                    break;
+                Entity entities = manager.CreateEntity(BlockArchetype);
+                manager.SetComponentData(entities, new Position { Value = new int3(xPos, i, zPos) });
+                manager.AddSharedComponentData(entities, new MeshInstanceRenderer
+                {
+                    mesh = blockMesh,
+                    material = woodMaterial
+                });
 
-                            }
-
-                            if (!airChecker)
+                //leaves
+                if(i >= yPos+3 && i <= yPos+6)
+                {
+                    for (int j = xPos - 1; j <= xPos + 1; j++)
+                    {
+                        for (int k = zPos - 1; k <= zPos + 1; k++)
+                        {
+                            if (k != zPos || j != xPos)
                             {
-                                Entity entities = manager.CreateEntity(BlockArchetype);
-                                manager.SetComponentData(entities, new Position { Value = new float3(xBlock, yBlock, zBlock) });
-                                //manager.SetSharedComponentData(entities[ordernumber], new MeshInstanceRenderer
-                                //{
-                                //  mesh = Meshtemp,
-                                //material = Matemp
-                                //});
 
+                                entities = manager.CreateEntity(BlockArchetype);
+                                manager.SetComponentData(entities, new Position { Value = new int3(j, i, k) });
                                 manager.AddSharedComponentData(entities, new MeshInstanceRenderer
                                 {
-                                    mesh = Meshtemp,
-                                    material = Matemp
+                                    mesh = blockMesh,
+                                    material = leavesMaterial
                                 });
-
                             }
-
-                            ordernumber += 1;
                         }
                     }
                 }
             }
-            //entities.Dispose();
         }
     }
 }
